a4a2c2ce86c4228c9c18cf20bc499197acd1b980
100BC <73491141+100BC@users.noreply.github.com>
2021-08-01 11:05:08 -0600

    Add Home Page Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import Layout from '@components/Layout';
+import styles from '@styles/home.module.scss';
 
 const Home = () => {
   return (
-    <Layout>
+    <Layout className={styles.home}>
       <h1>Hello World</h1>
     </Layout>
   );

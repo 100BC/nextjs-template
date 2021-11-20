35e8075c0029e9b24afb02ef5717ffbd0ccfded1
100BC <73491141+100BC@users.noreply.github.com>
2021-11-20 09:55:36 -0700

    Remove eslint-disable

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,14 +23,12 @@ class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#00aba9" />
           <meta name="theme-color" content="#ffffff" />
-          {/* eslint-disable react/no-invalid-html-attribute */}
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4c45de" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
-          {/* eslint-enable react/no-invalid-html-attribute */}
           {/* GOOGLE FONT */}
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
